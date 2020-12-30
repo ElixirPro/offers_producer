e29622c2bd695fade2740e51f189eb395c7b3f91
Gustavo Oliveira <g.92oliveira@gmail.com>
2020-12-30 07:32:26 -0300

    update deps

--- mix.exs
+++ mix.exs
@@ -22,7 +22,8 @@ defmodule OffersProducer.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:broadway, "~> 0.6.0"},
+      # {:broadway, "~> 0.6.0"},
+      {:brod, "~> 3.15"},
       {:jason, "~> 1.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
